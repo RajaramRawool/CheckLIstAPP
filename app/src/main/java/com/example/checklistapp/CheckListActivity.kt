61617e4bf37d0a18244f84281b78330736e81f1d
Rajaram Rawool <rajaram903@gamil.com>
2022-03-06 21:15:17 +0530

    Checklist basic layout

--- app/src/main/java/com/example/checklistapp/CheckListActivity.kt
+++ app/src/main/java/com/example/checklistapp/CheckListActivity.kt
@@ -3,9 +3,13 @@ package com.example.checklistapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CheckListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_checklist)
+
+
+
+
     }
 }
