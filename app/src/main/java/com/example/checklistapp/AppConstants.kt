b0097e74d8fa1a9efbd6b6f0d9c7ab39c9f684e7
Rajaram Rawool <rajaram903@gamil.com>
2022-03-18 05:15:58 +0530

    App related constants declaration

---
+++ app/src/main/java/com/example/checklistapp/AppConstants.kt
@@ -0,0 +1,15 @@
+package com.example.checklistapp
+
+const val APP_NAME: String = "CheckList App"
+const val USER_NAME: String = "USER_NAME"
+const val EMAIL_ADDRESS: String = "EMAIL_ADDRESS"
+const val PASSWORD: String = "PASSWORD"
+const val CONFIRM_PASSWORD: String = "CONFIRM_PASSWORD"
+const val USER_ID:String = "USER_ID"
+
+//Db related Constants
+const val DB_NAME: String = "u307181413_ChecklistAppDb"
+const val USER_TABLE_NAME: String = "users"
+const val ITEM_TABLE_NAME: String = "items"
+
+
