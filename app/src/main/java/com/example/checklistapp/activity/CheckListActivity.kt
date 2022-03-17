c6f0d272a73abe5524b382e7147d7d4a225640bb
Rajaram Rawool <rajaram903@gamil.com>
2022-03-18 04:15:42 +0530

    commit 1

--- app/src/main/java/com/example/checklistapp/activity/CheckListActivity.kt
+++ app/src/main/java/com/example/checklistapp/activity/CheckListActivity.kt
@@ -7,55 +7,19 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.checklistapp.appdatabase.AppDbHelper
 import com.example.checklistapp.databinding.ActivityChecklistBinding
-import com.example.checklistapp.model.Item
 
 class CheckListActivity : AppCompatActivity() {
     lateinit var binding: ActivityChecklistBinding
-    var itemList = mutableListOf<Item>()
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityChecklistBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        var db = AppDbHelper(this)
-        db.writeData("Milk")
 
 
-//        listeners
-        setListeners()
-//        Adapter for Recycler view
-//
-
-
-
-
-    }
-
-
-    private fun setListeners() {
-//        listener to add items
-        binding.btAddItem.setOnClickListener {
-            var itemName: String = binding.etItem.text.toString()
-            var checklistItem = Item(itemName)
-            itemList.add(checklistItem)
-            Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
-
-            var rvChecklist: RecyclerView = binding.rvChecklists
-            rvChecklist.adapter = CheckListAdapter(this, itemList)
-            rvChecklist.layoutManager = LinearLayoutManager(this)
-        }
 
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 
--- app/src/main/java/com/example/checklistapp/model/Item.kt
+++
@@ -1,4 +0,0 @@
-package com.example.checklistapp.model
-
-class Item(var name: String, var isChecked: Boolean = false) {
-}
