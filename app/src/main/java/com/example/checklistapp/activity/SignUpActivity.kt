64fb57b423f5ae37dbb203f781a4d7ed9e2d8cd6
Rajaram Rawool <rajaram903@gamil.com>
2022-03-19 05:47:31 +0530

    sign up page user input validation and insertion in live db

--- app/src/main/java/com/example/checklistapp/activity/SignUpActivity.kt
+++ app/src/main/java/com/example/checklistapp/activity/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.example.checklistapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Toast
 import com.example.checklistapp.appdatabase.Api
 import com.example.checklistapp.databinding.ActivitySignUpBinding
@@ -23,17 +24,95 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-        binding.btSignup.setOnClickListener{
-            val call = Api.retrofitService.insertUser("Rajaram","rajaram903@gmail.com","1234")
+        binding.btSignup.setOnClickListener {
+            val name = binding.etName.text.toString().trim()
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            val confirmPassword = binding.etConfirmPassowrd.text.toString().trim()
+           if (userInputValidation(name, email, password, confirmPassword)){
+
+            val call = Api.retrofitService.insertUser(name,email,password)
             call.enqueue(object : Callback<User?> {
                 override fun onResponse(call: Call<User?>, response: Response<User?>) {
                     Toast.makeText(this@SignUpActivity,"Success",Toast.LENGTH_LONG).show()
                 }
 
                 override fun onFailure(call: Call<User?>, t: Throwable) {
-                    Toast.makeText(this@SignUpActivity,t.message,Toast.LENGTH_LONG).show()
+                    Toast.makeText(this@SignUpActivity,"Failed:-${t.message}",Toast.LENGTH_LONG).show()
                 }
             })
+           }
         }
+    }
+
+    private fun userInputValidation(
+        name: String,
+        email: String,
+        password: String,
+        confirmPassword: String
+    ): Boolean {
+        if (isValidName(name)
+            && isValidEmail(email)
+            && isValidPassword(password)
+            && isValidConfirmPassword(password,confirmPassword)) {
+            return true
+        }
+        return false
+    }
+
+    private fun isValidConfirmPassword(password: String, confirmPassword: String): Boolean {
+        var result = false
+
+        when {
+            confirmPassword.isEmpty() -> binding.etConfirmPassowrd.error =
+                "Repeat Password to Confirm"
+
+            password != confirmPassword -> binding.etConfirmPassowrd.error =
+                "Password doesn't match"
+
+            else -> result = true
+        }
+        return result
+    }
+
+    private fun isValidPassword(password: String): Boolean {
+        var result = false
+
+        when {
+            password.isEmpty() -> binding.etPassword.error = "Enter Password"
+
+            (password.length < 4) || (password.length > 8) -> binding.etPassword.error =
+                "Length Should be between 4 to 8"
+
+            else -> result = true
+        }
+        return result
+
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        var result = false
+        when {
+            email.isEmpty() -> binding.etEmail.error = "Enter Email"
+
+            !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> binding.etEmail.error =
+                "Enter Valid Email"
+
+            else -> result = true
+        }
+        return result
+    }
+
+    private fun isValidName(name: String): Boolean {
+        var result = false
+        when {
+            name.isEmpty() -> binding.etName.error = "Enter name"
+
+            (name.length < 4) || (name.length > 8) -> binding.etName.error =
+                "Length should be between 4 to 8"
+
+            else -> result = true
+        }
+        return result
     }
 }
